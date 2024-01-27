b9dbb9d20c5254f59ef43b8b7b92de5771b0eb7b
TexanDoomGuy <ooofvill@gmail.com>
2024-01-26 21:58:20 -0600

    first commit

---
+++ sketch.js
@@ -0,0 +1,198 @@
+let player;
+let ground;
+let ground2;
+let ground3
+let img;
+var a
+var playery
+let jumpsound;
+let img2
+let follower
+let img3
+let img4
+let img5
+let ground5
+var peepee
+function preload() {
+  img = loadImage("4daca85b-2c06-4d53-a399-2a582590480c.png");
+  img2 = ("pressarrowkeystomove.png")
+  img3 = ("pressspace.png")
+  img4 = ("tutorial1.png")
+  img5 = ("slidin2.png")
+}
+
+function setup() {
+  world.gravity.y = 10;
+  //createCanvas(1347, 877);
+  createCanvas();
+  ground = new Sprite([
+    [300, 500],
+    [600, 500],
+    [900, 465],
+    [900, 415],
+  ]);
+  ground3 = new Sprite([
+    [100, 450],
+    [100, 700],
+    [950, 700],
+    [1400, 415],
+    [1750, 415]
+  ])
+  ground5 = new Sprite([
+    [100, 450],
+    [-28300, 450]
+  ])
+  ground6 = new Sprite([
+    [900, 415],
+    [900, 300]
+  ])
+  ground7 = new Sprite([
+    [100, 200],
+    [100, 450]
+  ])
+  strokeWeight(50)
+  ground4 = new Sprite([
+    [1250, 300], [1750, 345]
+  ])
+  ground6.collider = "static"
+  ground4.collider = "static"
+  ground3.collider = "static"
+  a = 1
+  ground.collider = "Static";
+  ground5.collider = "static";
+  ground7.collider = "static";
+  strokeWeight(4);
+  noStroke();
+  player = new Sprite();
+  player.image = img;
+  player.vel.x = 1;
+  player.scale = 1;
+  player.diameter = 50;
+  player.rotationDrag = 5;
+  player.x = 575
+  player.y = 300
+
+  ground2 = new Sprite()
+  ground2.y = 625;
+  ground2.w = 200;
+  ground2.collider = "static";
+  ground2.h = 5;
+  ground2.x = 600
+  text("player.x.vel = " + Math.round(player.vel.x), 10, 10, 200, 200);
+  textSize(20);
+  player.vel.y = 10
+  follower = new Sprite()
+  follower.img = "IGNORE THIS.png"
+  console.log("ignore the error, that's suppost to happen")
+  follower.collider = "none"
+  buttonprompt = new Sprite()
+  buttonprompt.img = "pressarrowkeystomove.png"
+  buttonprompt.collider = "none"
+  buttonprompt.x = 600
+  buttonprompt.y = 300
+
+  buttonprompt2 = new Sprite()
+  buttonprompt2.img = img3
+  buttonprompt2.collider = "none"
+  buttonprompt2.x = 600
+  buttonprompt2.y = 409
+  buttonprompt2.scale = 0.5
+
+  tutorial = new Sprite()
+  tutorial.img = img4
+  tutorial.collider = "none"
+  tutorial.y = 600
+  tutorial.x = 330
+  tutorial.scale = 0.3
+
+  slidin = new Sprite()
+  slidin.img = img5
+  slidin.collider = "none"
+  slidin.x = 1500
+  slidin.scale = 0.3
+  slidin.y = 300
+  a = 45
+}
+function draw() {
+  follower.moveTowards(player)
+  camera.x = follower.x
+  camera.y = follower.y
+  ground.friction = 1;
+  //player.bounciness = 0;  dont use, doesn't make the jump code work
+  //a++
+  if (player.x > 1000 & peepee != true){
+    ground7.collider = "None"
+    peepee = true
+    for (let i = 0; i < 45; i++) {
+      a ++
+    ground2.y = 600; }
+  }
+  ground2.rotation = a
+  background("rgb(99,99,99)");
+  colorMode(RGB, 255);
+  ground.color = "RGB(0,255,0)";
+  ground2.color = "RGB(0,0,255)"
+  ground3.color = "RGB(0,255,0)"
+  ground4.color = "RGB(0,0,255)"
+  ground5.color = "RGB(255,255,0)"
+  ground6.color = "RGB(255,0,0)"
+  ground7.color = "RGB(255,0,0)"
+
+  if (kb.pressing("left")) {
+    player.vel.x = player.vel.x - 0.50;
+    if (player.vel.x < -9) {
+      player.vel.x = player.vel.x + 0.40;
+    }
+  } else if (kb.pressing("right")) {
+    player.vel.x = player.vel.x + 0.50
+    if (player.vel.x > 9) {
+      player.vel.x = player.vel.x - 0.40;
+    }
+  } if (kb.pressing("space")) {
+    player.moveTowards(mouse);
+  } if (kb.pressing("z")) {
+    if (player.colliding(ground) >= 1) {
+      player.vel.y = -5
+    } else if (player.colliding(ground2) >= 1) {
+      player.vel.y = -5
+    } else if (player.colliding(ground3) >= 1) {
+      player.vel.y = -5
+    } else if (player.colliding(ground4) >= 1) {
+      player.vel.y = -5
+    } else if (player.colliding(ground5) >= 1) {
+      player.vel.y = -5
+    }
+      if (player.y >= 800) {
+        player.x = 600
+        player.y = 300
+      }
+
+      if (player.vel.y < 0.001 & player.vel.y > 0.01) {
+        player.vel.t = 0.4
+      }
+  }
+
+  player.y = player.y + 0
+  player.x = player.x + 0
+  text("arrow keys to move, and z to jump", 1300, 10, 500, 500)
+  text("player.x.vel = " + Math.round(player.vel.x), 10, 10, 200, 200);
+  text("player.y.vel = " + Math.round(player.vel.y), 10, 60, 200, 200);
+  text("player.rotation = " + Math.round(player.rotation), 10, 110, 200, 200);
+  text("player.x = " + Math.round(player.x), 10, 160, 200, 200);
+  text("player.y = " + Math.round(player.y), 10, 210, 200, 200);
+  text("player.colliding(ground) = " + player.colliding(ground), 10, 260, 200, 200)
+  text("player.colliding(ground2) = " + player.colliding(ground2), 10, 310, 200, 200)
+  text("player.colliding(ground3) = " + player.colliding(ground3), 10, 360, 200, 200)
+  text("player.colliding(ground4) = " + player.colliding(ground4), 10, 410, 200, 200)
+  text("player.colliding(ground5) = " + player.colliding(ground5), 10, 460, 200, 200)
+  text("a = ) = " +a, 10, 510, 200, 200)
+  //text("Camera x = "+camera(),10,360,200,200)
+}
+function keyPressed() {
+  if (keyCode == 81) {
+    player.x = 600
+    player.y = 300
+  }
+}
+
+// todo: make a level loading system
